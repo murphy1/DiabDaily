f5b669c9c2e62d67481295f43ad32be470014ab7
Stephen Murphy <smurphy54321@gmail.com>
2018-10-02 15:31:47 -0400

    added method for user to add their sugar levels
    integrated Googles Firebase for a cloud based db

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 # Diabetes application. Tracks sugar levels and food intake. Can also search foods to check nutritional values
 import requests
 import json
+import firebase_admin
+from firebase_admin import db
+from firebase_admin import credentials
+
+# Database information (Firebase)
+path_to_db = ""
+db_url = ""
 
 
 class DiabDaily(object):
 
-    # init method uses the Nutritionix API to search for the user inputted food
-    def __init__(self, app_id, app_key, food_input):
+    # init method initializes the Nutritionix API
+    def __init__(self, app_id, app_key):
+        self.headers = {"x-app-id": app_id, "x-app-key": app_key, "x-remote-user-id": "0"}  # 0 for Development
+
+        cred = credentials.Certificate(path_to_db)
+        firebase_admin.initialize_app(cred, {'databaseURL': db_url})
+        self.root = db.reference()
+
+    def nutritionix(self, food_input):
         item_id = ""
         url = "https://trackapi.nutritionix.com/v2/search/instant?query=%s" % food_input
-        headers = {"x-app-id": app_id, "x-app-key": app_key, "x-remote-user-id": "0"}  # 0 for Development
 
-        food_input_request = requests.get(url, headers=headers)
+        food_input_request = requests.get(url, headers=self.headers)
         food_input_request.raise_for_status()
         search_food_input_request = json.loads(food_input_request.text)
         food_input_request.close()
@@ -23,15 +36,17 @@ class DiabDaily(object):
                         item_id += v2
 
         item_url = "https://trackapi.nutritionix.com/v2/search/item?nix_item_id=%s" % item_id
-        item_info_request = requests.get(item_url, headers=headers)
+        item_info_request = requests.get(item_url, headers=self.headers)
         item_info_request.raise_for_status()
-        self.query_nutrients = json.loads(item_info_request.text)
+        query_nutrients = json.loads(item_info_request.text)
         item_info_request.close()
+        return query_nutrients
 
     # This method will use the food id from init method to return the nutritional values
-    def get_nutrients(self):
+    def get_nutrients(self, food_input):
+        query_nutrients = DiabDaily.nutritionix(self, food_input=food_input)
         print_list = []
-        for k3, v3 in self.query_nutrients.items():
+        for k3, v3 in query_nutrients.items():
             print_list.append("Similar Brand: "+v3[0]['brand_name'])
             print_list.append("Serving Size: "+str(v3[0]['serving_qty'])+" "+str(v3[0]['serving_unit']))
             print_list.append("Calories: "+str(v3[0]['nf_calories']))
@@ -40,3 +55,11 @@ class DiabDaily(object):
             print_list.append("Protein: "+str(v3[0]['nf_protein'])+"g")
         return print_list
 
+    # this method allows the user to enter their blood sugar levels
+    def sugar(self, reading, name):
+        # will add a new attribute to the db
+        new_user = self.root.child('Sugar Levels').push({
+            'level': '%s' % reading,  #mmol/L
+            'name': '%s' % name
+        })
+        print("%s's reading added!" % name)
